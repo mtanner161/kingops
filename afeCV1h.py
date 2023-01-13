fe3c1d1d8405a62d6a212fc2684b84a6d82de897
mtanner161 <m.tanner161@gmail.com>
2023-01-12 21:06:55 -0600

    adding looping through all files in folder and creating lists for each of the three needs: cost, hours, depth

--- afeCV1h.py
+++ afeCV1h.py
@@ -13,39 +13,66 @@ import re
 from dotenv import load_dotenv
 import pandas as pd
 import numpy as np
+from openpyxl import Workbook
 
-fileName = pd.read_csv(r".\kingops\data\afe\kinga199cv1h\1.9.2023.csv")
-fileNameExcel = pd.read_excel(
-    r".\kingops\data\afe\kinga199cv1h\1.1.2023test.xlsx")
+#fileName = pd.read_csv(r".\kingops\data\afe\kinga199cv1h\1.9.2023.csv")
+
+
+pathCv1hAfe = r".\kingops\data\afe\kinga199cv1h"
+folderList = os.listdir(pathCv1hAfe)
+
 
 costItemListClean = []
 
-fileName = fileName.fillna(0)
-dateList = fileName["textbox58"].tolist()
-costList = fileName["textbox13.1"].tolist()
-costItemList = fileName["textbox34"].tolist()
-totalMeasuredDepthList = fileName["Textbox8.1"].tolist()
-trueVerticalDepthList = fileName["Textbox10.1"].tolist()
+
+totalCostAllFile = []
+totalDateAllFile = []
+totalDepthAllFile = []
+
+for name in folderList:
+    fullPathFileName = pathCv1hAfe + "\\" + name
+    data = pd.read_csv(fullPathFileName)
+    data = data.fillna(0)
+    dateList = data["textbox58"].tolist()
+    costList = data["textbox13.1"].tolist()
+    costItemList = data["textbox34"].tolist()
+    totalMeasuredDepthList = data["Textbox8.1"].tolist()
+    trueVerticalDepthList = data["Textbox10.1"].tolist()
 # getting total / measured depth
-totalMeasuredDepth = totalMeasuredDepthList[0]
-trueVerticalDepth = trueVerticalDepthList[0]
-trueVerticalDepthClean = int(trueVerticalDepth.replace(",", ""))
-totalMeasuredDepthClean = int(totalMeasuredDepth.replace(",", ""))
+    totalMeasuredDepth = totalMeasuredDepthList[0]
+    if type(totalMeasuredDepth) == float or type(totalMeasuredDepth) == int:
+        totalMeasuredDepthClean = totalMeasuredDepth
+    else:
+        totalMeasuredDepthClean = float(totalMeasuredDepth.replace(",", ""))
 
-for i in range(0, len(costItemList)):
-    itemString = costItemList[i]
-    if itemString == 0:
-        itemString = ""
-    cleanString = itemString[5:]
-    costItemListClean.append(cleanString)
+    for i in range(0, len(costItemList)):
+        itemString = costItemList[i]
+        if itemString == 0:
+            itemString = ""
+        cleanString = itemString[5:]
+        costItemListClean.append(cleanString)
 
-costListClean = list(dict.fromkeys(costList))
+    costListClean = list(dict.fromkeys(costList))
+    totalDailyCost = 0
 
-columns = fileName.columns
-print(columns)
-print(type(costItemList[3]))
-totalDailyCost = sum(costListClean)
-dateOfAfe = dateList[0]
+    for j in range(0, len(costListClean)):
+        item = costListClean[j]
+        if type(item) == str:
+            item = item.replace(",", "")
+            item = item.replace("$", "")
+            totalDailyCost = totalDailyCost + float(item)
+        else:
+            totalDailyCost = totalDailyCost + item
+
+    columns = data.columns
+    print(columns)
+    print(type(costItemList[3]))
+    #totalDailyCost = sum(float(costListClean))
+    dateOfAfe = dateList[0]
+
+    totalCostAllFile.append(totalDailyCost)
+    totalDateAllFile.append(dateOfAfe)
+    totalDepthAllFile.append(totalMeasuredDepthClean)
 
 
 print("yay")
