4179161f754fbe0ac2ae4c69123fcc92cedfd23c
mtanner161 <m.tanner161@gmail.com>
2023-01-26 09:30:15 -0600

    updating to fix bugs

--- afeVarience.py
+++ afeVarience.py
@@ -14,15 +14,15 @@ from dotenv import load_dotenv
 import pandas as pd
 import numpy as np
 
+# import files needed
 budgetRawFile = pd.read_excel(
     r"C:\Users\mtanner\OneDrive - King Operating\Documents 1\code\kingops\data\afe\thurman23v\thurman23vAfeOg.xlsx")
-
 masterMatchFile = pd.read_excel(
     r".\kingops\data\afe\welldriveWolfepakMatch.xlsx")
-
 actualWellCostWolfepak = pd.read_excel(
     r"C:\Users\mtanner\OneDrive - King Operating\Documents 1\code\kingops\data\afe\thurman23v\thurman23vActualSpend.xlsx")
 
+# create lists from files
 welldriveBudgetAccounts = masterMatchFile["Code WellDrive"].tolist()
 wolfepakActualAccounts = masterMatchFile["Code WolfePak"].tolist()
 wolfepakActualDesc = masterMatchFile["Description WolfePak"].tolist()
@@ -31,25 +31,30 @@ actualAccountCodeList = actualWellCostWolfepak["Account"].tolist()
 actualAccountDescList = actualWellCostWolfepak["{Account Desc}"].tolist()
 actualCostList = actualWellCostWolfepak["Amount"].tolist()
 
+# create empty lists needed for loop
 outputData = []
-currentAccountCodes = []
+accountCodeInBudgetList = []
 
 fp = open(r"C:\Users\mtanner\OneDrive - King Operating\Documents 1\code\kingops\data\afe\thurman23v\thurman23vAfeActualVarience.csv", "w")
 
 headerString = "Account,Account Description,AFE Budget,Actual Spend,Varience\n"
 fp.write(headerString)
 
+# Master loop in order to match budget to actual
 for i in range(0, len(budgetRawFile)):
     # Budget row
     rowAfeBudgetRaw = budgetRawFile.iloc[i]
     afeBudgetAccountCode = rowAfeBudgetRaw["Account"]
     afeBudgetCost = rowAfeBudgetRaw["Cost"]
 
+    # gets index of buget account code in actual account code
     indexBudget = welldriveBudgetAccounts.index(afeBudgetAccountCode)
+    # matches with actual account code
     actualAccountCode = wolfepakActualAccounts[indexBudget]
     accountActualOccurrences = [j for j, x in enumerate(
         actualAccountCodeList) if x == actualAccountCode]
 
+    # handling multiple transactions with same account code
     if accountActualOccurrences == []:
         actualCostClean = 0
     else:
@@ -62,33 +67,21 @@ for i in range(0, len(budgetRawFile)):
     outputData.append([actualAccountCode, wolfepakActualDesc[indexDesc],
                       afeBudgetCost, actualCostClean])
 
-    currentAccountCodes.append(actualAccountCode)
+    accountCodeInBudgetList.append(actualAccountCode)
 
-for k in range(0, len(actualAccountCodeList)):
-    if actualAccountCodeList[k] not in currentAccountCodes:
-        outputData.append(
-            [actualAccountCodeList[k], actualAccountDescList[k], 0, actualCostList[k]])
 
 outputData.sort(key=lambda x: x[0])
 counter = 0
 
-tom = 1
-
-while tom == 1:
+trigger = True
+while trigger == True:
     account = outputData[counter][0]
     occurences = [j for j, x in enumerate(outputData) if x[0] == account]
     budgetCost = 0
     for m in range(0, len(occurences)):
         budgetCost += outputData[occurences[m]][2]
 
-    actualCost = 0
-    for m in range(0, len(occurences)):
-        actualCost += outputData[occurences[m]][3]
-
-    counter = occurences[len(occurences) - 1] + 1
-
-    junk = 1
-
+    actualCost = outputData[counter][3]
     varience = budgetCost - actualCost
 
     description = str(outputData[counter][1])
@@ -105,11 +98,62 @@ while tom == 1:
         + str(varience)
         + "\n"
     )
-
-    if counter == len(outputData) - 1:
-        tom = 0
-
     fp.write(printString)
+
+    counter = occurences[len(occurences) - 1] + 1
+
+    if counter == len(outputData):
+        trigger = False
+
+
+outputDataExtra = []
+
+for i in range(0, len(actualWellCostWolfepak)):
+    accountCodeActual = actualWellCostWolfepak.iloc[i]["Account"]
+    if accountCodeActual not in accountCodeInBudgetList:
+        accountActualOccurrences = [j for j, x in enumerate(
+            actualAccountCodeList) if x == accountCodeActual]
+
+        actualCostClean = 0
+        for m in range(0, len(accountActualOccurrences)):
+            actualCostClean += actualCostList[accountActualOccurrences[m]]
+
+        outputDataExtra.append(
+            [accountCodeActual, actualWellCostWolfepak.iloc[i]["{Account Desc}"], 0, actualCostClean])
+
+
+counter = 0
+trigger = True
+while trigger == True:
+    account = outputDataExtra[counter][0]
+    occurences = [j for j, x in enumerate(outputDataExtra) if x[0] == account]
+
+    budgetCost = 0
+
+    actualCost = outputDataExtra[counter][3]
+    varience = budgetCost - actualCost
+
+    description = str(outputDataExtra[counter][1])
+    betterDesc = description.replace(",", "")
+    printString = (
+        str(account)
+        + ","
+        + str(betterDesc)
+        + ","
+        + str(budgetCost)
+        + ","
+        + str(actualCost)
+        + ","
+        + str(varience)
+        + "\n"
+    )
+    fp.write(printString)
+
+    counter = occurences[len(occurences) - 1] + 1
+
+    if counter == len(outputDataExtra):
+        trigger = False
+
 
 fp.close()
 
