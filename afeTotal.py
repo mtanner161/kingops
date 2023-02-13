058dbd309bc3a48e794059ebd17b3af6f62ef98a
mtanner161 <m.tanner161@gmail.com>
2023-02-13 14:14:33 -0600

    combining afeVariacne and afeTotal into one export

--- afeTotal.py
+++ afeTotal.py
@@ -15,21 +15,22 @@ import pandas as pd
 import numpy as np
 from openpyxl import Workbook
 
-nameOfWell = "porter33v"
+nameOfWell = "chunn972v"
 
 pathOfDailyReport = '.\\kingops\\data\\afe' + '\\' + nameOfWell + "\\" + "daily"
 pathOfAfe = r".\kingops\data\afe" + "\\" + nameOfWell
-
 plannedCostFile = pathOfAfe + "\\" + nameOfWell + "planned.xlsx"
 plannedCostDepth = pd.read_excel(plannedCostFile)
-
+budgetRawString = pathOfAfe + "\\" + nameOfWell + "AfeOg.xlsx"
+actualSpendString = pathOfAfe + "\\" + nameOfWell + "ActualSpend.xlsx"
 masterMatchFile = pd.read_excel(
     r".\kingops\data\afe\welldriveWolfepakMatch.xlsx")
-
 pathOfMasterFile = pathOfAfe + "\\" + "fullreport.xlsx"
-
 masterAfe = pd.read_excel(pathOfMasterFile)
+actualWellCostWolfepak = pd.read_excel(actualSpendString)
+budgetRawFile = pd.read_excel(budgetRawString)
 
+wolfepakActualDesc = masterMatchFile["Description WolfePak"].tolist()
 welldriveBudgetAccounts = masterMatchFile["Code WellDrive"].tolist()
 wolfepakActualAccounts = masterMatchFile["Code WolfePak"].tolist()
 wellEzAccounts = masterMatchFile["Code Wellez"].tolist()
@@ -95,7 +96,7 @@ for i in range(0, len(masterAfe)):
     cleanDescription = description[5:]
     cleanDescription = cleanDescription.replace(",", "")
     measuredDepth = row[70]
-
+    activity = row[30]
     account = row[114]
     accountClean = int(abs(account))
     if accountClean > 0 and cost != 0:
@@ -124,7 +125,7 @@ for i in range(0, len(masterAfe)):
         totalDailyCost = totalDailyCost + cost
     else:
         cumulativeCost = cumulativeCost + totalDailyCost
-        if foundStartDate == 1:
+        if foundStartDate == 1 and activity == "Drilling":
             row = plannedCostDepth.iloc[day]
             lastDateClean = lastDate.strftime("%m/%d/%Y")
             outputString = (
@@ -209,5 +210,141 @@ for i in range(day + 1, len(plannedCostDepth)):
 dailyItemCostFp.close()
 daysVsDepthFp.close()
 
+print("Done with Days vs. Depth / Daily Cost Item")
 
-print("yay")
+# Begin AFE Variance
+
+actualAccountCodeList = actualWellCostWolfepak["Account"].tolist()
+actualAccountDescList = actualWellCostWolfepak["{Account Desc}"].tolist()
+actualCostList = actualWellCostWolfepak["Amount"].tolist()
+
+# create empty lists needed for loop
+outputData = []
+accountCodeInBudgetList = []
+
+filePointerString = pathOfAfe + "\\" + nameOfWell + "AfeActualVarience.csv"
+
+fp = open(filePointerString, "w")
+
+headerString = "Account,Account Description,AFE Budget,Actual Spend,Varience\n"
+fp.write(headerString)
+
+# Master loop in order to match budget to actual
+for i in range(0, len(budgetRawFile)):
+    # Budget row
+    rowAfeBudgetRaw = budgetRawFile.iloc[i]
+    afeBudgetAccountCode = rowAfeBudgetRaw["Account"]
+    afeBudgetCost = rowAfeBudgetRaw["Cost"]
+
+    # gets index of buget account code in actual account code
+    indexBudget = welldriveBudgetAccounts.index(afeBudgetAccountCode)
+    # matches with actual account code
+    actualAccountCode = wolfepakActualAccounts[indexBudget]
+    accountActualOccurrences = [j for j, x in enumerate(
+        actualAccountCodeList) if x == actualAccountCode]
+
+    # handling multiple transactions with same account code
+    if accountActualOccurrences == []:
+        actualCostClean = 0
+    else:
+        actualCostClean = 0
+        for m in range(0, len(accountActualOccurrences)):
+            actualCostClean += actualCostList[accountActualOccurrences[m]]
+
+    indexDesc = wolfepakActualAccounts.index(actualAccountCode)
+
+    outputData.append([actualAccountCode, wolfepakActualDesc[indexDesc],
+                      afeBudgetCost, actualCostClean])
+
+    accountCodeInBudgetList.append(actualAccountCode)
+
+
+outputData.sort(key=lambda x: x[0])
+counter = 0
+
+trigger = True
+while trigger == True:
+    account = outputData[counter][0]
+    occurences = [j for j, x in enumerate(outputData) if x[0] == account]
+    budgetCost = 0
+    for m in range(0, len(occurences)):
+        budgetCost += outputData[occurences[m]][2]
+
+    actualCost = outputData[counter][3]
+    varience = budgetCost - actualCost
+
+    description = str(outputData[counter][1])
+    betterDesc = description.replace(",", "")
+    printString = (
+        str(account)
+        + ","
+        + str(betterDesc)
+        + ","
+        + str(budgetCost)
+        + ","
+        + str(actualCost)
+        + ","
+        + str(varience)
+        + "\n"
+    )
+    fp.write(printString)
+
+    counter = occurences[len(occurences) - 1] + 1
+
+    if counter == len(outputData):
+        trigger = False
+
+
+outputDataExtra = []
+
+for i in range(0, len(actualWellCostWolfepak)):
+    accountCodeActual = actualWellCostWolfepak.iloc[i]["Account"]
+    if accountCodeActual not in accountCodeInBudgetList:
+        accountActualOccurrences = [j for j, x in enumerate(
+            actualAccountCodeList) if x == accountCodeActual]
+
+        actualCostClean = 0
+        for m in range(0, len(accountActualOccurrences)):
+            actualCostClean += actualCostList[accountActualOccurrences[m]]
+
+        outputDataExtra.append(
+            [accountCodeActual, actualWellCostWolfepak.iloc[i]["{Account Desc}"], 0, actualCostClean])
+
+
+counter = 0
+trigger = True
+while trigger == True:
+    account = outputDataExtra[counter][0]
+    occurences = [j for j, x in enumerate(outputDataExtra) if x[0] == account]
+
+    budgetCost = 0
+
+    actualCost = outputDataExtra[counter][3]
+    varience = budgetCost - actualCost
+
+    description = str(outputDataExtra[counter][1])
+    betterDesc = description.replace(",", "")
+    printString = (
+        str(account)
+        + ","
+        + str(betterDesc)
+        + ","
+        + str(budgetCost)
+        + ","
+        + str(actualCost)
+        + ","
+        + str(varience)
+        + "\n"
+    )
+    fp.write(printString)
+
+    counter = occurences[len(occurences) - 1] + 1
+
+    if counter == len(outputDataExtra):
+        trigger = False
+
+
+fp.close()
+
+
+print("Done with AFE Varience")
